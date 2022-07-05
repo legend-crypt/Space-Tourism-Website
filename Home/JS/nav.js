01bc15d54543b749dfcd31c02a83df01f7155867
Konadu Lord Kweku <yrnlehjend@gmail.com>
2022-07-05 19:31:19 +0000

    add close button

--- Home/JS/nav.js
+++ Home/JS/nav.js
@@ -6,8 +6,12 @@ navToggle.addEventListener("click",()=>{
 
     if(visibility ==="false"){
         primaryNav.setAttribute("data-visible",true);
+        navToggle.setAttribute('aria-expanded',true);
+
     }
     else if(visibility ==="true"){
-        primaryNav.setAttribute("data-visible",false)
+        primaryNav.setAttribute("data-visible",false);
+        navToggle.setAttribute('aria-expanded',false);
+
     }
 });
